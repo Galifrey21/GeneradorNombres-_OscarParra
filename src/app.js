37e066f365b72f7267d96a4ccd8d822a28e6c4c1
Oscar Parra <oscarparrapalomino@gmail.com>
2024-11-25 16:09:34 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominios = [".com", ".es", ".net"];
+
+  let nombre = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dominios.length; l++) {
+          nombre = pronoun[i] + adj[j] + noun[k] + dominios[l];
+          document.getElementById("generado").innerHTML += nombre + "<br>";
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
